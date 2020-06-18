355c1374385710be3d297d60d2616de66e72a0ee
mmcguire91 <mmcguire91@gmail.com>
2020-06-18 18:04:19 -0500

    Incorporated coinAPI and retrieved data for BTC:USD

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,8 @@
+import 'package:http/http.dart' as http;
+//requires user to specify package.DataType
+import 'dart:convert';
+//imports jsonDecode
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +33,31 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const coinApiUrl = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = '6EF3769F-22FF-4F28-94F8-27E64A8ED337';
+
+class CoinData {
+  //Create the Asynchronous method getCoinData() that returns a Future (the price data).
+
+  Future getCoinData() async {
+    //Create a url combining the coinAPIURL with the currencies we're interested, BTC to USD
+    String url = '$coinApiUrl/BTC/USD?apikey=$apiKey';
+    //Make a GET request to the URL and wait for the response.
+    http.Response response = await http.get(url);
+
+    //Check that the request was successful.
+    if (response.statusCode == 200) {
+      //Use the 'dart:convert' package to decode the JSON data that comes back from coinapi.io.
+      var decodedData = jsonDecode(response.body);
+      //Get the last price of bitcoin with the key 'last'.
+      var lastPrice = decodedData['rate'];
+      //Output the lastPrice from the method.
+      return lastPrice;
+    } else {
+      //Handle any errors that occur during the request.
+      print(response.statusCode);
+      //throw an error if our request fails.
+      throw 'Problem with the get request';
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -54,11 +54,36 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
+  //Create a variable to hold the value and use in our Text Widget. Give the variable a starting value of '?' before the data comes back from the async methods.
+  String bitcoinValueInUSD = '?';
+
+  //Create an async method here await the coin data from coin_data.dart
+  void getData() async {
+    try {
+      double data = await CoinData().getCoinData();
+      //We can't await in a setState(). So you have to separate it out into two steps.
+      setState(() {
+        bitcoinValueInUSD = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+    //Call getData() when the screen loads up
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('🤑 Coin Ticker'),
+        title: Center(
+          child: Text('🤑 Coin Ticker'),
+        ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -75,7 +100,8 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  //Update the Text Widget with the live bitcoin data here.
+                  '1 BTC = $bitcoinValueInUSD USD',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
